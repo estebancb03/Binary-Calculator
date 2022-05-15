d4465d79bfea38a106f461174459a98949189655
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-05-14 18:43:53 -0600

    Se crean headers de métodos de validación

--- src/validate.h
+++ src/validate.h
@@ -1,11 +1,16 @@
 #ifndef VALIDATE_H
 #define VALIDATE_H
-
+#include <iostream>
+#include <string.h>
+using namespace std;
 
 class Validate {
     public:
         Validate();
         ~Validate();
+        int validateAll(string operation, string numbers[], string type[]);
+        int validateBinary(string number);
+        int validateDecimal(string number);
 };
 
 #endif // VALIDATE_H

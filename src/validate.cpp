c0ea5c6244bd677af0b8a300cd6e3b058da3edc1
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-05-14 20:27:13 -0600

    Código de métodos de validación

--- src/validate.cpp
+++ src/validate.cpp
@@ -1,5 +1,62 @@
 #include "validate.h"
+#include <iostream>
 
 Validate::Validate() {
 
+}
+
+int Validate :: push(string operation, string* numbers, string* type) {
+    this -> operation = operation;
+    this -> numbers = numbers;
+    this -> type = type;
+    return EXIT_SUCCESS;
+}
+
+int Validate :: pull() {
+    int error = EXIT_SUCCESS;
+    int size = sizeof this -> numbers / sizeof this -> numbers[0];
+    if (this -> operation == "NOT ( ~ )") {
+        if (size != 1)
+            error = EXIT_FAILURE;
+        else {
+            error = validateNumber(numbers[0]);
+            if (this -> type[0] == "Binary")
+                error = validateBinary(numbers[0]);
+        }
+    } else {
+        if (size != 2)
+            error = EXIT_FAILURE;
+        for (int index = 0; index < size && error == EXIT_SUCCESS; ++index) {
+            error = validateNumber(this -> numbers[index]);
+            if (this -> type[index] == "Binary")
+                error = validateBinary(this -> numbers[index]);
+        }
+    }
+    return error;
+}
+
+int Validate :: validateBinary(string number) {
+    int error = EXIT_SUCCESS;
+    for (int index = 0; number[index] && error == EXIT_SUCCESS; ++index) {
+        int currentNumber = (int) number[index];
+        if (currentNumber != 0 && currentNumber != 1)
+            error = EXIT_FAILURE;
+    }
+    return error;
+}
+
+int Validate :: validateNumber(string number) {
+    int error = EXIT_SUCCESS;
+    if(number[0] == '-') {
+        for (int index = 1; number[index] && error == EXIT_SUCCESS; ++index) {
+            if (!isdigit(number[index]))
+                error = EXIT_FAILURE;
+        }
+    } else {
+        for (int index = 0; number[index] && error == EXIT_SUCCESS; ++index) {
+            if (!isdigit(number[index]))
+                error = EXIT_FAILURE;
+        }
+    }
+    return error;
 }
--- src/validate.h
+++ src/validate.h
@@ -1,16 +1,20 @@
 #ifndef VALIDATE_H
 #define VALIDATE_H
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 
 class Validate {
+    string* type;
+    string* numbers;
+    string operation;
     public:
         Validate();
         ~Validate();
-        int validateAll(string operation, string numbers[], string type[]);
+        int push(string operation, string* numbers, string* type);
+        int pull();
         int validateBinary(string number);
-        int validateDecimal(string number);
+        int validateNumber(string number);
 };
 
 #endif // VALIDATE_H

5f1d026461a0d1ef07d5cee2a13f5f669310a998
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-07-25 23:56:54 -0600

    Eliminación Controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include "mainwindow.h"
-#include "controller.h"
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    //Controller* controller = new Controller();
     MainWindow* window = new MainWindow(nullptr);
     window -> show();
     return a.exec();
